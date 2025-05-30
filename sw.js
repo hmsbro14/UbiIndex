3a767710aae9e79b3fa8c17a572d724ea4bac280
hmsbro14 <harrisonmikesmith14@gmail.com>
2025-05-30 10:53:02 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'webapp-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.PNG' // remove this line if you're not using an icon
+];
+
+// Install: cache everything in FILES_TO_CACHE
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate: cleanup old caches if any
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch: respond with cache first, then network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
